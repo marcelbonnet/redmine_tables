0d6fa86916f3aae868a5818f4e09768cbee6af73
Marcel Bonnet <marcelbonnet@anatel.gov.br>
2021-11-11 16:25:59 -0300

    WIP: Table Memberships

---
+++ app/controllers/table_memberships_controller.rb
@@ -0,0 +1,24 @@
+class TableMembershipsController < ApplicationController
+  layout 'admin'
+  self.main_menu = false
+
+  before_action :find_membership, :only => [:edit]
+
+  def edit
+    @roles = Role.givable.to_a
+    respond_to do |format|
+      format.js
+      format.html
+    end
+  end
+
+  def find_membership
+    begin
+      @membership = TableMember.find(params[:id])
+      @group = Group.find(params[:custom_table_id]) # na verdade é group_id
+    rescue ActiveRecord::RecordNotFound
+      render_404
+    end
+  end
+
+end
